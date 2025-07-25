bdf0daf9ac4c59a1591ce3d4c08ef7c8ee1d0f4e
Mark Saroufim <marksaroufim@meta.com>
2025-07-24 20:16:06 -0700

    Clean up remaining build artifacts and update dependencies

---
+++ pytorch-load-inline-highlighter/src/extension.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+function activate(context) {
+    // Extension activated - no additional setup needed for grammar injection
+}
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
